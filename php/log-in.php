6a37c319e1356498787cacbb41cca37a46010f8a
Jarel Binggan <jarelbinggan881@gmail.com>
2025-12-09 00:50:13 +0800

    new

---
+++ php/log-in.php
@@ -0,0 +1,32 @@
+<?php
+require "config.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $sql->bind_param("s", $email);
+    $sql->execute();
+    $result = $sql->get_result();
+
+    if ($result->num_rows === 0) {
+        echo "NOUSER";
+        exit;
+    }
+
+    $user = $result->fetch_assoc();
+
+    if (!password_verify($password, $user["password"])) {
+        echo "WRONGPASS";
+        exit;
+    }
+
+    // Save user session
+    $_SESSION["user_id"] = $user["user_id"];
+    $_SESSION["user_name"] = $user["first_name"];
+
+    echo "SUCCESS";
+}
+?>
